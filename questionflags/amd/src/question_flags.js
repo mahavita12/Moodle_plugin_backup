4702963589d38aa7d9032cbad2047ccbcb7cc936
mahavita12 <mahavita@gmail.com>
2025-10-30 17:32:58 +1100

    install.xml update

--- questionflags/amd/src/question_flags.js
+++
@@ -1,6 +0,0 @@
-// AMD module not currently used - functionality implemented via hook system
-// See: classes/hook/output/before_footer_html_generation.php
-define([], function() {
-    'use strict';
-    return {};
-});
---
+++ questionflags_off/amd/src/question_flags.js
@@ -0,0 +1,6 @@
+// AMD module not currently used - functionality implemented via hook system
+// See: classes/hook/output/before_footer_html_generation.php
+define([], function() {
+    'use strict';
+    return {};
+});
